029e399b51d2d0f9169d138be9f240c5c3f78ea7
Ashwin Aashu <80351416+AshwinAashu@users.noreply.github.com>
2022-01-28 10:04:32 +0530

    controlModal

--- src/components/modals/NewPostModal.jsx
+++ src/components/modals/NewPostModal.jsx
@@ -2,6 +2,17 @@ const NewPostModal = ({ showPostModal }) => {
   if (!showPostModal) {
     return null;
   }
-  return <div>something</div>;
+  return (
+    <div className="newPostModal-container">
+      <div className="newPostModal-header"></div>
+      <div className="newPostModal-buttons">
+        <button className="btn-secondary">GIF</button>
+        <div className="newPostModal-controls">
+          <button className="btn-secondary">Cancel</button>
+          <button className="btn-primary">Post</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default NewPostModal;
